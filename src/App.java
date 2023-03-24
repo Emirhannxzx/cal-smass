5adac9fca6f9c29fc3832998c6a70fa25a184fac
Emirhannxzx <emirhan.imamogluu.34@gmail.com>
2023-03-24 11:57:47 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Ucgen ucgen = new Ucgen(5, 6);
+        System.out.println(ucgen.Alan());
+        
+    }
+}
